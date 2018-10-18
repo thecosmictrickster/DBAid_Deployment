baf120511a51d3d12c124c2d6b02520b090e1684
thecosmictrickster <thecosmictrickster@users.noreply.github.com>
2018-10-19 11:58:09 +1300

    Think this is finally done.

--- fn_Get-ClusterSQLNames.ps1
+++ fn_Get-ClusterSQLNames.ps1
@@ -1,9 +1,11 @@
 <#  
  .Synopsis 
-  Queries WMI to see if the current machine is in a cluster and returns the Windows cluster name.
+  Queries WMI via custom function to see if the current machine is in a cluster and returns SQL Virtual Network Names (VNNs).
  
  .Description 
-  This function takes the Windows cluster name and queries WMI to retrieve SQL cluster VNN(s). 
+  This function gets the Windows cluster name via the custom function Get-ClusterWindowsName and uses PowerShell FailoverCluster functions to retrieve SQL cluster VNN(s). 
+  If the server is not clustered, return result will advise so.
+  If the server is clustered but no clustered instances of SQL Server are detected, (e.g. AlwaysOn AG scenario), return result will advise so.
  
  .Example 
   Get-ClusterSQLVNNs
@@ -17,13 +19,14 @@ function Get-ClusterSQLVNNs {
 
   if ($clusterwindowsname -ine "NotClustered") {
     Import-Module FailoverClusters
-    $sqlinstancenames = Get-ClusterResource -cluster $clusterwindowsname | ? { $_.ResourceType -like "SQL Server"} | Get-ClusterParameter -cluster $clusterwindowsname VirtualServerName,InstanceName | Group-Object ClusterObject | Select @{ Name = "SQLInstance";Expression = { [string]::join("\",($_.Group | Select -expandproperty Value)) } } | Out-String
-    if ($sqlinstancenames[0] -ieq "") {
-      $sqlinstancenames[0] = "NotSQLCluster"
+    # need to split custom object gets split into a string array. Otherwise you get a list of instances in a single variable. Filter out headers & blank lines as well.
+    $sqlinstancenames = (Get-ClusterResource -cluster $clusterwindowsname | ? { $_.ResourceType -like "SQL Server"} | Get-ClusterParameter -cluster $clusterwindowsname VirtualServerName,InstanceName | Group-Object ClusterObject | Select @{ Name = "SQLInstance";Expression = { [string]::join("\",($_.Group | Select -expandproperty Value)) } } | Out-String).Split("`n") | ? { ($_ -like "[a-zA-Z0-9]*") -and ($_ -notlike "SQLInstance*") }
+    if ($sqlinstancenames.Count -lt 1) {
+      $sqlinstancenames = "NotSQLCluster"
     }
   }
   else {
-    $sqlinstancenames[0] = "NotClustered"
+    $sqlinstancenames = "NotClustered"
   }
   return $sqlinstancenames
 }
